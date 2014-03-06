1768de65f424e33393f6a2d85e73c872da2f4a4b
Tommy Martin <gravity226@gmail.com>
2014-03-06 11:35:10 -0500

    Test Rails 4 push

---
+++ tommy_test/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TommyTest::Application.config.secret_key_base = '69aaa0d44ed4d607f025cb8512a534ee2e562e20bce815999c18d231ba8da5f889e52b1265728dcc572855962fd855719eadb7217afbf1634b7c72bd0fc46512'
---
+++ tommy_test/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TommyTest::Application.config.session_store :cookie_store, key: '_tommy_test_session'
